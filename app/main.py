a4835f983110cbcfcf2fc3c76344d084a4d30240
Abhinav Kunal <abhinavkunal@Abhinavs-MacBook-Pro.local>
2022-02-23 01:17:51 +0800

    2nd commit

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ import time
 from . import models
 from .database import engine
 from fastapi import Depends, FastAPI
-from .routers import post,user
+from .routers import post,user,auth
    
 models.Base.metadata.create_all(bind = engine)
 
@@ -40,6 +40,7 @@ def find_index_post(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 @app.get("/")
 def root():
