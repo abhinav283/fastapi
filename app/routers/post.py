9c6be441fcc149ca482de26a31182ae9c1fbb8ce
Abhinav Kunal <abhinavkunal@Abhinavs-MacBook-Pro.local>
2022-02-25 20:26:34 +0800

    2nd commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -13,7 +13,8 @@ router = APIRouter(
 def get_posts(db:Session=Depends(get_db),current_user:int = Depends(oauth2.get_current_user)):
     # cursor.execute("""select * from posts""" )
     # posts=cursor.fetchall()
-    posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
+    posts = db.query(models.Post).all()
     return posts
 
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
@@ -34,8 +35,8 @@ def get_post(id:int,db:Session=Depends(get_db),current_user:int = Depends(oauth2
     post = db.query(models.Post).filter(models.Post.id==id).first()    
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id : {id} not found")
-    if post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Not authorized to get Post" )
+    # if post.owner_id != current_user.id:
+    #     raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail=f"Not authorized to get Post" )
     return post
 
 @router.delete("/{id}",status_code=status.HTTP_204_NO_CONTENT ) 
