4a470dc71dcf04e9cf5f10422bbcb91f979c27b1
Abhinav Kunal <abhinavkunal@Abhinavs-MacBook-Pro.local>
2022-02-25 23:54:45 +0800

    2nd commit

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,6 +4,7 @@ from . import schemas,database,models
 from fastapi import Depends,status,HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 
@@ -11,9 +12,9 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 #alogirth HS256
 #expiration time
 
-SECRET_KEY =  'c0e7be332b91d9832d0cd5fab1e44e27188946ca5b2efe0669135b595528faea'
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data:dict):
     to_encode =  data.copy()
