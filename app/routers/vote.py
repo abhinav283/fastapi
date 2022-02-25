9b83f0c5d9949c97087aa54161333b649cc5a74b
Abhinav Kunal <abhinavkunal@Abhinavs-MacBook-Pro.local>
2022-02-26 01:10:42 +0800

    2nd commit

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -10,6 +10,12 @@ router = APIRouter(
 
 @router.post("/",status_code=status.HTTP_201_CREATED)
 def vote(vote:schemas.Vote,db : Session = Depends(database.get_db),current_user: int = Depends(oauth2.get_current_user)):
+
+    post = db.query(models.Post).filter(models.Post.id == vote.post_id).first()
+    print(post)
+    if not post:
+       raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Post with id {vote.post_id} doesnt exist') 
+
     vote_query = db.query(models.Vote).filter(models.Vote.post_id==vote.post_id,models.Vote.user_id == current_user.id)
     found_vote = vote_query.first()
     if (vote.dir ==1):
@@ -24,4 +30,4 @@ def vote(vote:schemas.Vote,db : Session = Depends(database.get_db),current_user:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'Vote doesnt exist')
         vote_query.delete(synchronize_session=False)
         db.commit()
-        return {'message':'succesfully deleted vote'}
+        return {'message':'succesfully deleted vote'}    
