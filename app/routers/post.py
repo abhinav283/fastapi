36b660a30f52f4eeebfbe9bc20b8d0bd3ec809c6
Abhinav Kunal <abhinavkunal@Abhinavs-MacBook-Pro.local>
2022-02-25 20:46:43 +0800

    2nd commit

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 from .. import models,schemas,oauth2
 from fastapi import FastAPI, Depends, Response,status,HTTPException,APIRouter
 from sqlalchemy.orm import Session
@@ -10,11 +10,11 @@ router = APIRouter(
 )
 
 @router.get("/",response_model=List[schemas.Post])
-def get_posts(db:Session=Depends(get_db),current_user:int = Depends(oauth2.get_current_user),Limit:int=10):
+def get_posts(db:Session=Depends(get_db),current_user:int = Depends(oauth2.get_current_user),limit:int=10,skip:int=0,search:Optional[str]=""):
     # cursor.execute("""select * from posts""" )
     # posts=cursor.fetchall()
     # posts = db.query(models.Post).filter(models.Post.owner_id == current_user.id).all()
-    posts = db.query(models.Post).limit(limit=Limit).all()
+    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit=limit).offset(skip).all()
     return posts
 
 @router.post("/",status_code=status.HTTP_201_CREATED,response_model=schemas.Post)
